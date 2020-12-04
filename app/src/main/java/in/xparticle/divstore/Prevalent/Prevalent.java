e8ade41af98080bd2a2a224ba00deeb62c46c00f
Dhananjaypathak27 <43525854+Dhananjaypathak27@users.noreply.github.com>
2020-12-04 13:23:42 +0530

    HomeActivity added and User remember feature added

---
+++ app/src/main/java/in/xparticle/divstore/Prevalent/Prevalent.java
@@ -0,0 +1,12 @@
+package in.xparticle.divstore.Prevalent;
+
+import in.xparticle.divstore.Model.Users;
+
+public class Prevalent {
+
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+
+}
