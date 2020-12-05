94f63907f5ea53b02619ffef908700429aa9075d
Dhananjaypathak27 <43525854+Dhananjaypathak27@users.noreply.github.com>
2020-12-05 12:27:25 +0530

    AdminAddNewProductActivity ui added

--- app/src/main/java/in/xparticle/divstore/AdminAddNewProductActivity.java
+++ app/src/main/java/in/xparticle/divstore/AdminAddNewProductActivity.java
@@ -3,11 +3,17 @@ package in.xparticle.divstore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class AdminAddNewProductActivity extends AppCompatActivity {
 
+    private Button AddNewProductButton;
     private String CategoryName;
+    private ImageView InputProductImage;
+    private EditText InputProductName,InputProductDescription,InputProductPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +22,11 @@ public class AdminAddNewProductActivity extends AppCompatActivity {
 
         CategoryName = getIntent().getStringExtra("category");
         Toast.makeText(this, ""+ CategoryName, Toast.LENGTH_SHORT).show();
+
+        AddNewProductButton = findViewById(R.id.add_new_product);
+        InputProductImage = findViewById(R.id.select_product_image);
+        InputProductName = findViewById(R.id.product_name);
+        InputProductDescription = findViewById(R.id.product_description);
+        InputProductPrice = findViewById(R.id.product_price);
     }
 }
