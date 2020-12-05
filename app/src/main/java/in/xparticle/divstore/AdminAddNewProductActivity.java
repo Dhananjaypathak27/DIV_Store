d7ee89840c1c901d2c7f85efd1dfa6d7e96eae7f
Dhananjaypathak27 <43525854+Dhananjaypathak27@users.noreply.github.com>
2020-12-05 11:35:10 +0530

    AdminCategoryActivity added and products to category

--- app/src/main/java/in/xparticle/divstore/AdminAddNewProductActivity.java
+++ app/src/main/java/in/xparticle/divstore/AdminAddNewProductActivity.java
@@ -7,11 +7,14 @@ import android.widget.Toast;
 
 public class AdminAddNewProductActivity extends AppCompatActivity {
 
+    private String CategoryName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_add_new_product);
 
-        Toast.makeText(this, "Admin panel", Toast.LENGTH_SHORT).show();
+        CategoryName = getIntent().getStringExtra("category");
+        Toast.makeText(this, ""+ CategoryName, Toast.LENGTH_SHORT).show();
     }
 }
