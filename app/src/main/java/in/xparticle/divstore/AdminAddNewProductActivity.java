976b2c4ecc28ee44a3d00573bf3a74aeb559d19e
Dhananjaypathak27 <43525854+Dhananjaypathak27@users.noreply.github.com>
2020-12-06 13:09:26 +0530

    AdminAddNewProductActivity - product added to storage and database functionality added

--- app/src/main/java/in/xparticle/divstore/AdminAddNewProductActivity.java
+++ app/src/main/java/in/xparticle/divstore/AdminAddNewProductActivity.java
@@ -1,19 +1,48 @@
-package in.xparticle.divstore;
+ package in.xparticle.divstore;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.SimpleTimeZone;
+
 public class AdminAddNewProductActivity extends AppCompatActivity {
 
     private Button AddNewProductButton;
-    private String CategoryName;
+    private String CategoryName,Description,Price,Pname,saveCurrentDate,saveCurrentTime;
     private ImageView InputProductImage;
     private EditText InputProductName,InputProductDescription,InputProductPrice;
+    private static final int GalleryPick = 1;
+    private Uri ImageUri;
+    private String productRandomKey, downloadImageURL;
+    private StorageReference productImagesRef;
+    private DatabaseReference ProductsRef;
+    private ProgressDialog loadingBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +51,159 @@ public class AdminAddNewProductActivity extends AppCompatActivity {
 
         CategoryName = getIntent().getStringExtra("category");
         Toast.makeText(this, ""+ CategoryName, Toast.LENGTH_SHORT).show();
+        productImagesRef = FirebaseStorage.getInstance().getReference().child("Product Images");
+        ProductsRef = FirebaseDatabase.getInstance().getReference().child("Products");
 
         AddNewProductButton = findViewById(R.id.add_new_product);
         InputProductImage = findViewById(R.id.select_product_image);
         InputProductName = findViewById(R.id.product_name);
         InputProductDescription = findViewById(R.id.product_description);
         InputProductPrice = findViewById(R.id.product_price);
+        loadingBar = new ProgressDialog(this);
+
+        InputProductImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGallery();
+            }
+        });
+
+        AddNewProductButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validateProductData();
+            }
+        });
+    }
+
+    private void validateProductData() {
+            Description= InputProductDescription.getText().toString();
+            Price = InputProductPrice.getText().toString();
+            Pname = InputProductName.getText().toString();
+
+            if(ImageUri==null){
+                Toast.makeText(this, "Product Image Is mandatory...", Toast.LENGTH_SHORT).show();
+            }
+            else if(Description.equals("")){
+                Toast.makeText(this, "Please write product Description...", Toast.LENGTH_SHORT).show();
+            }
+            else if(Price.equals("")){
+                Toast.makeText(this, "Please write product price...", Toast.LENGTH_SHORT).show();
+            }
+            else if(Pname.equals("")){
+                Toast.makeText(this, "Please write product name...", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                storeProductInformation();
+            }
+    }
+
+    private void storeProductInformation() {
+
+        loadingBar.setTitle("Add New product");
+        loadingBar.setMessage("Please wait, while we are Adding new Products...");
+        loadingBar.setCanceledOnTouchOutside(false);
+        loadingBar.show();
+        Calendar calendar = Calendar.getInstance();
+
+        SimpleDateFormat currentDate = new SimpleDateFormat("MMM dd, yyyy");
+        saveCurrentDate = currentDate.format(calendar.getTime());
+
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+        saveCurrentTime = currentTime.format(calendar.getTime());
+
+        productRandomKey = saveCurrentDate + saveCurrentTime;
+
+        StorageReference filePath = productImagesRef.child(ImageUri.getLastPathSegment() + productRandomKey + ".jpg");
+
+        final UploadTask uploadTask = filePath.putFile(ImageUri);
+
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                String message = e.getMessage();
+                Toast.makeText(AdminAddNewProductActivity.this, "Error" + message, Toast.LENGTH_SHORT).show();
+                loadingBar.dismiss();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(AdminAddNewProductActivity.this, "Product Image Uploaded Successfully...", Toast.LENGTH_SHORT).show();
+
+                Task<Uri> uriTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                    @Override
+                    public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                        if(!task.isSuccessful()){
+                            throw task.getException();
+                        }
+
+                        downloadImageURL = filePath.getDownloadUrl().toString();
+                        return filePath.getDownloadUrl();
+
+                    }
+                }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Uri> task) {
+                        if(task.isSuccessful()){
+
+                            downloadImageURL = task.getResult().toString();
+                            Toast.makeText(AdminAddNewProductActivity.this, "got the product image Url Successfully...", Toast.LENGTH_SHORT).show();
+
+                            saveInfoToDatabase();
+
+                        }
+                    }
+                });
+
+
+            }
+        });
+    }
+
+    private void saveInfoToDatabase() {
+
+        HashMap<String ,Object> productMap = new HashMap<>();
+        productMap.put("pid",productRandomKey);
+        productMap.put("date",saveCurrentDate);
+        productMap.put("time",saveCurrentTime);
+        productMap.put("description",Description);
+        productMap.put("image",downloadImageURL);
+        productMap.put("category",CategoryName);
+        productMap.put("price",Price);
+        productMap.put("pname",Pname);
+
+        ProductsRef.child(productRandomKey).updateChildren(productMap)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            finish();
+                            loadingBar.dismiss();
+                            Toast.makeText(AdminAddNewProductActivity.this, "Product is added successfully...", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            loadingBar.dismiss();
+                            String message = task.getException().toString();
+                            Toast.makeText(AdminAddNewProductActivity.this, "Error"+ message, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+
+    private void openGallery() {
+        Intent galleryIntent = new Intent();
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent,GalleryPick);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==GalleryPick && resultCode==RESULT_OK && data!=null){
+                ImageUri = data.getData();
+                InputProductImage.setImageURI(ImageUri);
+        }
     }
 }
